9a17744e3704964a5637551e2285bb32fec43316
MarioSomodi <52134229+MarioSomodi@users.noreply.github.com>
2021-02-13 19:03:00 +0100

    Add files via upload

---
+++ LV1-Basic calculator in Windows Forms app/Zadatak_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculation;
+
+namespace Zadatak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int odabir;
+            double result = 0, x, y;
+            Console.WriteLine("Unesite dva broja.");
+            Console.Write("Unesi prvi broj: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Unesi drugi broj: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1.Zbrajanje\n2.Mnozenje\n3.Oduzimanje\n4.Djeljenje");
+            Console.Write("Odabir: ");
+            odabir = Convert.ToInt32(Console.ReadLine());
+            Calculator Casio = new Calculator();
+            switch (odabir)
+            {
+                case 1: 
+                    {
+                        result = Casio.Add(x, y);
+                        Console.WriteLine($"{x} + {y} = {result}");
+                        break;
+                    }
+                case 2: 
+                    {
+                        result = Casio.Multiply(x, y);
+                        Console.WriteLine($"{x} * {y} = {result}");
+                        break;
+                    }
+                case 3:
+                    {
+                        result = Casio.Subtract(x, y);
+                        Console.WriteLine($"{x} - {y} = {result}");
+                        break;
+                    }
+                case 4:
+                    {
+                        result = Casio.Divide(x, y);
+                        Console.WriteLine($"{x} / {y} = {result}");
+                        break;
+                    }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
