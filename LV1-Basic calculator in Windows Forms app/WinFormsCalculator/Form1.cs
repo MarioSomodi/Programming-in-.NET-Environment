2d22cf910d9c88d972e213711160829e3fed20f0
MarioSomodi <52134229+MarioSomodi@users.noreply.github.com>
2021-02-13 19:03:33 +0100

    Add files via upload

---
+++ LV1-Basic calculator in Windows Forms app/WinFormsCalculator/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculation;
+
+namespace WinFormsCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Calculator Casio = new Calculator();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double x = Casio.Add(Convert.ToDouble(inptFirstNumber.Value), Convert.ToDouble(inptSecondNumber.Value));
+            inptResult.Text = $"{x}";
+        }
+
+        private void btnSubstract_Click(object sender, EventArgs e)
+        {
+            double x = Casio.Subtract(Convert.ToDouble(inptFirstNumber.Value), Convert.ToDouble(inptSecondNumber.Value));
+            inptResult.Text = $"{x}";
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            double x = Casio.Multiply(Convert.ToDouble(inptFirstNumber.Value), Convert.ToDouble(inptSecondNumber.Value));
+            inptResult.Text = $"{x}";
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            double x = Casio.Divide(Convert.ToDouble(inptFirstNumber.Value), Convert.ToDouble(inptSecondNumber.Value));
+            inptResult.Text = $"{x}";
+        }
+    }
+}
